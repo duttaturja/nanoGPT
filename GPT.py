158cce8a2fe2b72e6d47d7eb6ade654673a497bb
duttaturja <duttaturja@gmail.com>
2025-03-20 23:15:56 +0600

    added training optimization to overfit and prevent loss

--- GPT.py
+++ GPT.py
@@ -177,18 +177,25 @@ text = text[:1000]
 tokens = enc.encode(text)
 B, T = 4, 32
 buf = torch.tensor(tokens[:B*T + 1])
+buf = buf.to(device)
 x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
 
 # get logits
 model = GPT(GPTConfig())
 model.to(device)
-logits, loss = model(x, y)
+# logits, loss = model(x, y)
 
-# print the logits
-logits = logits[0] 
-print(logits.shape)
-print(loss)
+# optimize!
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item():.4f}")
+
+
 import sys; sys.exit(0)
 
 
