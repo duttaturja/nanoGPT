b3892a6f628a1a0f0719ebd2713d571aecb849d1
duttaturja <duttaturja@gmail.com>
2025-05-21 21:11:51 +0600

    configuration and attention model

---
+++ nanoGPT/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence size
+    vocab_size: int = 50257 # number of tokens
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
---
+++ nanoGPT/model.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import inspect
+
+# Casual Self-Attention Layer
+class CasualSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0, "Embedding dimension must be divisible by number of heads"
+        # q, k, v projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+    def forward(self, x):
+        # batch size, sequence length, embedding dimension
+        B, T, C = x.size()
+        # query, key, value projection
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        # flash attention
+        flash_attn = F.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=0.0)
+        flash_attn = flash_attn.transpose(1, 2).contiguous().view(B, T, C)
+
+        # output projection
+        flash_attn = self.c_proj(flash_attn)
+        return flash_attn
+    
+# Multi-Layer Perceptron for Feed Forward Network
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        # regularization parameter
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+    
+# Basic transformer block
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
