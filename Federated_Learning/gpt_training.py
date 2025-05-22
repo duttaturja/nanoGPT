999e871f20e57a1ff1d8ad102960b265663de48c
duttaturja <duttaturja@gmail.com>
2025-05-23 01:05:18 +0600

    Federated training

---
+++ Federated_Learning/gpt_training.py
@@ -0,0 +1,26 @@
+import torch
+from torch.nn import CrossEntropyLoss
+from torch.optim import AdamW
+
+
+def train_one_epoch(model, dataloader, device, lr):
+    model.train()
+    model.to(device)
+    opt = AdamW(model.parameters(), lr=lr)
+    loss_fn = CrossEntropyLoss()
+
+    for x, y in dataloader:
+        x, y = x.to(device), y.to(device)
+
+        logits = model(x)
+        if isinstance(logits, tuple):           # <-- handle tuple output
+            logits = logits[0]
+
+        loss = loss_fn(
+            logits.view(-1, logits.size(-1)),
+            y.view(-1),
+        )
+
+        opt.zero_grad(set_to_none=True)
+        loss.backward()
+        opt.step()
